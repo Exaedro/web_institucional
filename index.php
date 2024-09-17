71a1c4da078ca70e24bb42e3b338bb55f630dde8
Exaedro <owomolo123@gmail.com>
2024-09-17 14:47:21 -0300

    pagina de inicio

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+	<title>Escuela de Educación Secundaria Nro1</title>
+</head>
+
+<body>
+	<?php include_once("navbar.php"); ?>
+
+	<main class="inicio">
+		<section class="lateral animate__animated animate__fadeInLeft">
+			<h2>Escuela de Educación Secundaria Tecnica Nro°1</h2>
+		</section>
+		<section class="secciones">
+			<article class="especialidades">
+				<p>Especialidades</p>
+				<div class="botones">
+					<a href="#">Electronica</a>
+					<a href="#">Programación</a>
+				</div>
+			</article>
+			<article class="celeste">
+				<p>Inscripciones</p>
+			</article>
+			<article class="celeste">
+				<p>Campos</p>
+			</article>
+			<article class="directivos negro">
+				<p>Directivos</p>
+			</article>
+		</section>
+	</main>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,18 @@
+<nav>
+    <header class="navbar-header">
+        <img
+            src="img/logo.png"
+            alt="Escuela de Educación Secundaria Nro1" />
+    </header>
+    <div class="navbar-body">
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Acerca de nosotros</a></li>
+            <li><a href="#">Directivos</a></li>
+            <li><a href="#">Porque la técnica</a></li>
+            <li><a href="#">Posgrado</a></li>
+            <li><a href="#">Estudiantes</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </div>
+</nav>
