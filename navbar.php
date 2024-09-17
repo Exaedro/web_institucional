4b53be2d84109ad1a0787b566c42c8c24cd4782d
franciscosucho <franciscosuchomela@gmail.com>
2024-09-17 17:08:08 -0300

    creacion del apartado sobre_nosotros

--- navbar.php
+++ navbar.php
@@ -1,13 +1,13 @@
 <nav>
     <header class="navbar-header">
         <img
-            src="img/logo.png"
+            src="./../img/logo.png"
             alt="Escuela de Educación Secundaria Nro1" />
     </header>
     <div class="navbar-body">
         <ul>
             <li><a href="#">Inicio</a></li>
-            <li><a href="#">Acerca de nosotros</a></li>
+            <li><a href="./paginas/sobre_nosotros.php">Acerca de nosotros</a></li>
             <li><a href="#">Directivos</a></li>
             <li><a href="#">Porque la técnica</a></li>
             <li><a href="#">Posgrado</a></li>
--- paginas/sobre_nosotros.php
+++ paginas/sobre_nosotros.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style_nosotro.css">
     <title>Escuela de Educación Secundaria Nro1</title>
 </head>
 
@@ -12,7 +13,23 @@
     <?php include_once("../navbar.php"); ?>
 
     <main>
-        <h1>adsdsadas</h1>
+
+        <div id="cont_img">
+            <img src="./../img/tecnica.jpg" alt="">
+            <h3 class="tit_item"></h3>
+            <span class="text"></span>
+        </div>
+        <div id="cont_text">
+            <h2>Quienes somos?</h2>
+            <ul id="cont_items">
+                <li class="item">Historia de la Escuela</li>
+                <li class="item">Fundacion y crecimiento</li>
+                <li class="item">Valores de la escuela</li>
+                <li class="item">Actualidad</li>
+                <li class="item">Personal y Asociación Cooperadora</li>
+                <li class="item">Código de Conducta</li>
+            </ul>
+        </div>
     </main>
 </body>
 
