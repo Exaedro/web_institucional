3db0f25bd39345146b242890a862208fd6358729
Agustin Colman <agusicolman@gmail.com>
2024-10-15 19:04:16 -0300

    actualizo inscripciones

--- inscrip.php
+++ inscrip.php
@@ -22,7 +22,7 @@
 
         function textPorque() {
             title.innerHTML = "¿POR QUÉ INSCRiBIRSE EN LA TÉCNICA?"    
-            info.innerHTML = "Adquirir habilidades en programación y electrónica es una inversión clave para el futuro. Estas competencias son esenciales en muchas industrias, desde la salud hasta el entretenimiento, y la demanda de profesionales en estas áreas sigue creciendo, lo que asegura oportunidades laborales atractivas y bien remuneradas. Más allá del ámbito laboral, aprender programación y electrónica desarrolla habilidades importantes como la creatividad y la resolución de problemas. Enfrentar desafíos técnicos potencia el pensamiento crítico y lógico, capacidades valiosas en cualquier campo."
+            info.innerHTML = "¿Te gusta resolver problemas y crear soluciones innovadoras? En nuestra escuela, contamos con los recursos necesarios para que puedas expresar tu creatividad. Tendrás acceso a computadoras, netbooks y herramientas de trabajo. Desde el primer día, estarás trabajando en proyectos, desde la creación de un horno solar, un semáforo con leds y de forma más avanzada la producción de circuitos electrónicos simples hasta aplicaciones web complejas. Esta experiencia te permitirá desarrollar habilidades clave como el pensamiento crítico, la resolución de problemas y el trabajo en equipo, preparándote para los desafíos del mundo profesional."
             porque.classList.add("active")   
             que.classList.remove("active") 
             cuando.classList.remove("active")        
@@ -37,7 +37,11 @@
         }  
         function textCuando() {
             title.innerHTML = "¿Cuándo se abren las inscripciones?"    
-            info.innerHTML = "3"  
+            info.innerHTML = "Las inscripciones se abren en el mes de Noviembre. Si le gustaria ver que proyectos fueron trabajados durante el año vengase a la Expotecnica de la escuela, en el cual, se muestran todos los proyectos de todos los cursos."  
+            function changeImage(){ 
+                const image = document.getElementById('myImage');
+                image.src = './img/mes.png';
+            }
             porque.classList.remove("active")   
             que.classList.remove("active") 
             cuando.classList.add("active")             
@@ -53,12 +57,15 @@
         <div class="btn-holder">
             <div class="btn" id="1">¿Por qué inscribirse?</div>
             <div class="btn" id="2">¿Que traer?</div>
-            <div class="btn" id="3"> ¿Cuando abren?</div>
+            <div class="btn" id="3" onclick="changeImage()"> ¿Cuando abren?</div>
         </div>
         <div class="content-holder">
                 <div class="content">
                 <h1 id="title">¿POR QUÉ INSCRiBIRSE EN LA TÉCNICA?</h1>
-                <p id="info">Adquirir habilidades en programación y electrónica es una inversión clave para el futuro. Estas competencias son esenciales en muchas industrias, desde la salud hasta el entretenimiento, y la demanda de profesionales en estas áreas sigue creciendo, lo que asegura oportunidades laborales atractivas y bien remuneradas. Más allá del ámbito laboral, aprender programación y electrónica desarrolla habilidades importantes como la creatividad y la resolución de problemas. Enfrentar desafíos técnicos potencia el pensamiento crítico y lógico, capacidades valiosas en cualquier campo.</p></div>
+                <p id="info">¿Te gusta resolver problemas y crear soluciones innovadoras? En nuestra escuela, contamos con los recursos necesarios para que puedas expresar tu creatividad. Tendrás acceso a computadoras, netbooks y herramientas de trabajo. Desde el primer día, estarás trabajando en proyectos, desde la creación de un horno solar, un semáforo con leds y de forma más avanzada la producción de circuitos electrónicos simples hasta aplicaciones web complejas. Esta experiencia te permitirá desarrollar habilidades clave como el pensamiento crítico, la resolución de problemas y el trabajo en equipo, preparándote para los desafíos del mundo profesional.
+                </p>
+                <img id="myImage" src="" alt="">
+            </div>
         </div>
     </main>
 </body>
