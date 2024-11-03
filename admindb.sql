ce5420afaa546c4fbd650d0291100a3a48ab1ed3
Tzaw18 <134438342+Tzaw14@users.noreply.github.com>
2024-11-03 16:08:58 -0300

    actualizacion de la BD con las fotos y descripciones DIR

--- admindb.sql
+++ admindb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-10-2024 a las 00:09:13
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 03-11-2024 a las 20:08:15
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `admin_user` (
   `id` int(11) NOT NULL,
   `user_name` varchar(33) NOT NULL,
   `password` varchar(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `admin_user`
@@ -51,29 +51,29 @@ CREATE TABLE `directivos` (
   `img_url` varchar(33) NOT NULL,
   `nombre` varchar(33) NOT NULL,
   `cargo` varchar(70) NOT NULL,
-  `descripcion` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `descripcion` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `directivos`
 --
 
 INSERT INTO `directivos` (`id_dir`, `img_url`, `nombre`, `cargo`, `descripcion`) VALUES
-(1, '', 'Ezequiel Torres', 'Director', 'Esta es la información completa del directivo. Aqu'),
-(2, '', 'Daniel Segnini', 'Vicedirector', 'Esta es la información completa del directivo. Aqu'),
-(3, '', 'Mariana Bonetti', 'Vicedirectora', 'Esta es la información completa del directivo. Aqu'),
-(4, '', 'Karin Cuervo', 'Vicedirectora', 'Esta es la información completa del directivo. Aqu'),
-(5, '', 'Esteban Lemos', 'Jefe de Area', 'Esta es la información completa del directivo. Aqu'),
-(6, '', 'Alejandro Hsia', 'Jefe de Departamento Técnico Profesional', 'Esta es la información completa del directivo. Aqu'),
-(7, '', 'Pablo Heredia', 'Jefe de Departamento de Educación Física', 'Esta es la información completa del directivo. Aqu'),
-(8, '', 'Nancy Dunjo', 'Jefa de Departamendo de Comunicaciones', 'Esta es la información completa del directivo. Aqu'),
-(9, '', 'Matias Gutierrez', 'Jefe de Departamento de Ciencias Naturales y Exactas', 'Esta es la información completa del directivo. Aqu'),
-(10, '', 'Diana Cozzani', 'Jefa de Departamento de Ciencias Sociales\r\n', 'Esta es la información completa del directivo. Aqu'),
-(11, '', 'Victorello', 'Jefa de Preceptoria', 'Esta es la información completa del directivo. Aqu'),
-(12, '', 'Cristina Goméz', 'Secretaría', 'Esta es la información completa del directivo. Aqu'),
-(13, '', 'Alejandra Basack', 'Prosecretaria', 'Esta es la información completa del directivo. Aqu'),
-(14, '', 'Martín Lopez', 'Encargado en Jefe y apoyo Informatico\r\n', 'Esta es la información completa del directivo. Aqu'),
-(15, '', 'Alejandro Miguel San Martín', 'Padrino', 'Esta es la información completa del directivo. Aqu');
+(1, 'img/Directivos/torres.png', 'Ezequiel Torres', 'Director', 'Es el encargado y responsable de la institución. Desempeña un papel multifacético que abarca desde el liderazgo y la visión hasta la gestión de recursos y la mejora del rendimiento estudiantil. Su objetivo principal es crear un entorno propicio para el aprendizaje y el desarrollo de los estudiantes, tanto académico como personal.'),
+(2, 'img/Directivos/segnini.png', 'Daniel Segnini', 'Vicedirector', 'Es el segundo encargado y responsable de la institución.'),
+(3, 'img/Directivos/bonetti.png', 'Mariana Bonetti', 'Vicedirectora', 'Es la segunda encargada y responsable de la institución.'),
+(4, 'img/Directivos/cuervo.png', 'Karin Cuervo', 'Vicedirectora', 'Es la segunda encargada y responsable de la institución.'),
+(5, 'img/Directivos/lemos.png', 'Esteban Lemos', 'Jefe de Area', 'Supervisa los talleres y coordina pasantías.'),
+(6, 'img/Directivos/hsia.png', 'Alejandro Hsia', 'Jefe de Departamento Técnico Profesional', 'Coordina las materias y actividades que se realizan en los talleres y laboratorios.'),
+(7, 'img/Directivos/nora.png', 'Nora Cavia', 'Jefa de Departamento de Educación Física', 'Es la encargada y responsable de las actividades y profesores del área.'),
+(8, 'img/Directivos/dunjo.png', 'Nancy Dunjo', 'Jefa de Departamendo de Comunicaciones', 'Planificar y coordinar proyectos entre distintas areas y/o departamentos ( interdiciplinarios e interdepartamentales) supervisar y acompañar a los docentes.'),
+(9, 'img/Directivos/gutierrez.png', 'Matias Gutierrez', 'Jefe de Departamento de Ciencias Naturales y Exactas', 'Es el encargado y responsable de la coordinación y realización de las materias de dicha área'),
+(10, 'img/Directivos/cozzani.png', 'Diana Cozzani', 'Jefa de Departamento de Ciencias Sociales\r\n', 'Es la encargada de la coordinación y organización de las materias y profesores del área de sociales.'),
+(11, 'img/Directivos/victorelo.png', 'Veronica Victorello', 'Jefa de Preceptoria', 'Es la encargada de la organización de las y los preceptores.'),
+(12, 'img/Directivos/gomez.png', 'Cristina Goméz', 'Secretaría', 'Es la encargada de las tareas administrativas de la institución.'),
+(13, 'img/Directivos/basack.png', 'Alejandra Basack', 'Prosecretaria', 'Es la segunda encargada de las tareas administrativas de la institución.'),
+(14, 'img/Directivos/lopez.png', 'Martín Lopez', 'Encargado en Jefe y apoyo Informatico\r\n', 'Es el encargado del mantenimiento y correcto funcionamiento de los equipos y servidores.'),
+(15, 'img/Directivos/miguelMartin.png', 'Alejandro Miguel San Martín', 'Padrino', 'Brinda su apoyo a la institución. apadrinada, ayudandola en todo lo que esté a su alcance');
 
 -- --------------------------------------------------------
 
@@ -86,7 +86,7 @@ CREATE TABLE `mail` (
   `nombre` varchar(33) NOT NULL,
   `mensaje` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `mail`
